80b8e21881e24833b318b159a54e757dce51b390
yavuz selim gozukara <78623551+yavuz-gozukara@users.noreply.github.com>
2022-12-13 11:01:53 +0300

    yavuz selim
    @themanoftalent

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,19 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 int main(){
- int i,j,k,n; 
+ int i,j,n,k=1; 
  printf("Enter the number of the row :");
  scanf("%d",&n);
  
- for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
+ for(i=0;i<=n;i++)
+ 	{
+    	for(j=0;j<=i;j++)
+		{
+     	printf(" yavuz");
+		 	
+ 		}
+ 		
+ 	printf("\n");
+	}
  
     
 }
